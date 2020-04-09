c6740b2e431e0acc2280ea4ec4a217eebd5ee785
Денис <denis.zhidkov01@gmail.com>
2020-04-09 13:16:11 +0400

    Fixed some problems

--- src/grepcode/Grep.java
+++ src/grepcode/Grep.java
@@ -3,84 +3,117 @@ package grepcode;
 import java.io.*;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.util.ArrayList;
 
 public class Grep {
 
-    private final String inputFileName;
+    private final Pattern reg;
+    private final boolean invert;
+    private final boolean ignore;
 
-    private final String word;
-
-    public Grep (String word, String inputFileName) {
-        this.word = word;
-        this.inputFileName = inputFileName;
+    public Grep (Pattern reg, boolean invert, boolean ignore) {
+        this.reg = reg;
+        this.invert = invert;
+        this.ignore = ignore;
     }
 
-    public void find(Pattern reg, int invert, int ignore) throws IOException {
+    public ArrayList<String> find(String word, String inputFileName) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(inputFileName));
         String line;
+        ArrayList<String> res = new ArrayList<>();
+        int i = 0;
         if (reg != null) {
-            if (invert != 1) {
-                if (ignore != 1) {
+            if (!invert) {
+                if (!ignore) {
                     while (reader.readLine() != null) {
                         line = reader.readLine();
                         Matcher matcher = reg.matcher(line);
-                        if (matcher.find()) System.out.println(line);
+                        if (matcher.find()) {
+                            res.add(i, line);
+                            i++;
+                        }
                     }
                 }
                 else {
                     while (reader.readLine() != null) {
                         line = reader.readLine().toLowerCase();
                         Matcher matcher = reg.matcher(line);
-                        if (matcher.find()) System.out.println(line);
+                        if (matcher.find()) {
+                            res.add(i, line);
+                            i++;
+                        }
                     }
                 }
             }
             else {
-                if (ignore != 1) {
+                if (!ignore) {
                     while (reader.readLine() != null) {
                         line = reader.readLine();
                         Matcher matcher = reg.matcher(line);
-                        if (!matcher.find()) System.out.println(line);
+                        if (!matcher.find()) {
+                            res.add(i, line);
+                            i++;
+                        }
                     }
                 }
                 else {
                     while (reader.readLine() != null) {
                         line = reader.readLine().toLowerCase();
                         Matcher matcher = reg.matcher(line);
-                        if (!matcher.find()) System.out.println(line);
+                        if (!matcher.find()) {
+                            res.add(i, line);
+                            i++;
+                        }
                     }
                 }
             }
         }
         else {
-            if (invert != 1) {
-                if (ignore != 1) {
-                    while (reader.readLine() != null) {
-                        line = reader.readLine();
-                        if (line.contains(word)) System.out.println(line);
+            if (!invert) {
+                if (!ignore) {
+                    while ((line = reader.readLine()) != null) {
+                        if (line.contains(word)) {
+                            res.add(i, line);
+                            i++;
+                        }
                     }
                 }
                 else {
                     while (reader.readLine() != null) {
                         line = reader.readLine().toLowerCase();
-                        if (line.contains(word)) System.out.println(line);
+                        if (line.contains(word)) {
+                            res.add(i, line);
+                            i++;
+                        }
                     }
                 }
             }
             else {
-                if (ignore != 1) {
-                    while (reader.readLine() != null) {
-                        line = reader.readLine();
-                        if (!line.contains(word)) System.out.println(line);
+                if (!ignore) {
+                    while ((line = reader.readLine()) != null) {
+                        if (!line.contains(word)) {
+                            res.add(i, line);
+                            i++;
+                        }
                     }
                 }
                 else {
                     while (reader.readLine() != null) {
                         line = reader.readLine().toLowerCase();
-                        if (!line.contains(word)) System.out.println(line);
+                        if (!line.contains(word)) {
+                            res.add(i, line);
+                            i++;
+                        }
                     }
                 }
             }
         }
+        return res;
     }
 }
+/* 1) Неправильная работа -i (выводятся строки в нижнем регистре);
+   2) Приложение не выводит одинаковые строки;
+   3)* (Возможно) Неправильная работа REGEX;
+   4) Неправильная работа -v (выводит не все строки);
+   5) Доделать тесты;
+ */
--- test/grepcode/GrepTest.java
+++ test/grepcode/GrepTest.java
@@ -13,7 +13,16 @@ public class GrepTest {
     public void find() throws IOException {
         Grep grep = new Grep(null, false, false);
         ArrayList<String> test = new ArrayList<>();
-        test.add("Не слово,");
+        /*test.add("Не слово,");
+        assertEquals(test, grep.find("слово", "out/artifacts/JavaConsoleUtility_jar/In1.txt"));*/
+
+        grep = new Grep(null, true, false);
+        //test.clear();
+        test.add("Слово");
+        test.add("Но");
+        test.add("Слово");
+        test.add("Есть");
+        test.add("Слово!");
         assertEquals(test, grep.find("слово", "out/artifacts/JavaConsoleUtility_jar/In1.txt"));
     }
 }
